7c35f1cd09782514de5bd2104f88a377a630879e
kkan9ma <kyeongmankang@gmail.com>
2025-09-13 16:40:05 +0900

    좌석 이동 기능 구현

--- week01/jazzclub/src/jazzclub/JazzClub.java
+++ week01/jazzclub/src/jazzclub/JazzClub.java
@@ -29,9 +29,14 @@ public class JazzClub {
 
         while (true) {
             System.out.println("원하는 좌석의 번호를 입력해 주세요.\n");
+            System.out.println("뒤로 돌아가기 원하신다면 0번을 입력해 주세요.\n");
             this.seat.showSeatsExcluding(NONE_SELECT);
 
             int selectedSeatNumber = sc.nextInt();
+
+            if (selectedSeatNumber == 0) {
+                break;
+            }
 
             if (!this.seat.isValidSeatNumber(selectedSeatNumber)) {
                 this.seat.printIsWrongSeatNumber(selectedSeatNumber);
@@ -62,15 +67,56 @@ public class JazzClub {
 
 
     public void handleChangeSeat() {
-        System.out.println("좌석 이동");
-        /*
-         *   [좌석 이동]
-         *   1. 이미 점유하고 있는 좌석이 있는지 확인 -> 없다면 에러 메시지
-         *   2. 현재 좌석을 제외한 가능한 좌석 보여주기
-         *   3. 원하는 좌석 번호 입력 받기
-         *   4. 기존 좌석 해제
-         *   5. 좌석 점유
-         * */
+        System.out.println("\n=====[좌석 이동]=====\n");
+
+        if (this.guest.getCurrentSeat() == NONE_SELECT) {
+            System.out.println("이미 보유한 좌석이 없습니다. 좌석 선택 메뉴를 이용해 주세요.");
+            return;
+        }
+
+        int currentSeat = this.guest.getCurrentSeat();
+
+        while (true) {
+            System.out.println("원하는 좌석의 번호를 입력해 주세요.\n");
+            System.out.println("뒤로 돌아가기 원하신다면 0번을 입력해 주세요.\n");
+            this.seat.showSeatsExcluding(currentSeat);
+
+            int selectedSeatNumber = sc.nextInt();
+
+            if (selectedSeatNumber == 0) {
+                break;
+            }
+
+            if (selectedSeatNumber == currentSeat) {
+                System.out.println("현재 좌석과 동일한 좌석입니다.");
+                continue;
+            }
+            if (!this.seat.isValidSeatNumber(selectedSeatNumber)) {
+                this.seat.printIsWrongSeatNumber(selectedSeatNumber);
+                continue;
+            }
+            if (!this.seat.isSeatAvailable(selectedSeatNumber)) {
+                System.out.println("이미 선점된 좌석입니다.");
+                continue;
+            }
+
+            this.seat.occupySeat(selectedSeatNumber);
+            System.out.println("좌석 선택이 완료되었습니다. 발급받은 입장권을 갖고 들어가 주세요.\n\n");
+            String ticket = String.format("""
+                    =====================================
+                    |               Ticket              |
+                    |-----------------------------------|
+                    | 좌석: %d번                            |
+                    |-----------------------------------|
+                    |  <Bluer Jazz Club>                |
+                    =====================================
+                    """, selectedSeatNumber);
+
+            System.out.println(ticket);
+            this.guest.setCurrentSeat(selectedSeatNumber);
+            this.seat.releaseSeat(currentSeat);
+            break;
+        }
     }
 
     public void handleOrder() {
