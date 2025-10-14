f3e2505615a23115c44cd6f7a50abdd5787aabbb
kkan9ma <kyeongmankang@gmail.com>
2025-10-14 16:30:31 +0900

    공통된 응답 형식을 구현하기 위한 ApiResponse 클래스 생성

---
+++ week04/main-assignment/community/src/main/java/com/example/community/common/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.example.community.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(staticName = "of")
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(message, data);
+    }
+
+    public static ApiResponse<Void> success(String message) {
+        return new ApiResponse<>(message, null);
+    }
+
+    public static ApiResponse<Void> error(String message) {
+        return new ApiResponse<>(message, null);
+    }
+}
