26cb75a904b78d08ff73617a2e3f3aef94f4b582
kkan9ma <kyeongmankang@gmail.com>
2025-10-16 17:30:20 +0900

    Comment Controller,Dto 보완

--- week04/main-assignment/community/src/main/java/com/example/community/controller/CommentController.java
+++ week04/main-assignment/community/src/main/java/com/example/community/controller/CommentController.java
@@ -8,17 +8,10 @@ import com.example.community.dto.CreateCommentRequest;
 import com.example.community.dto.UpdateCommentRequest;
 import com.example.community.service.CommentService;
 import jakarta.validation.Valid;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/post/{postId}/comments")
--- week04/main-assignment/community/src/main/java/com/example/community/dto/CreateCommentRequest.java
+++ week04/main-assignment/community/src/main/java/com/example/community/dto/CreateCommentRequest.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 public class CreateCommentRequest {
 
-    @NotBlank
+    @NotBlank(message = "댓글은 공백일 수 없습니다.")
     private String content;
 }
